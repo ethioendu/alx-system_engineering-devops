321adbb158622a47186196d957f536882742aa90
root <ethioendu@git.com>
2024-03-23 05:00:35 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 
 # Using Puppet, create a manifest that kills a process named killmenow.
 
-exec { 'pkill':
+exec { 'kill':
   command  => 'pkill killmenow',
-  path    => ['/usr/bin', '/usr/sbin'] 
+    provider => 'shell',
+ 
 }
