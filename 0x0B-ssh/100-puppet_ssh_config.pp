be2a16a95fec511d352d40198d066196d1be8acd
root <ethioendu@git.com>
2024-03-24 07:22:38 -0700

    git

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,19 @@
-#create ssh config file
-#configuration must be configured to use the private key ~/.ssh/school
-#configuration must be configured to refuse to authenticate using a password
-class ssh_config {
-  file { '/home/vagrant/.ssh/config':
-    ensure  => present,
-    mode    => '0600',
-    content => "
-      Host your_server
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-    ",
-    owner   => 'vagrant',
-    group   => 'vagrant',
-  }
+# using Puppet to make changes to our configuration file.
+# Requirements:Your SSH client configuration must
+#   configured to use the private key ~/.ssh/school
+#   configured to refuse to authenticate using a password.
+
+include stdlib
+
+File_line { 'Turn off passwd auth':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => 'true',
+}
+
+File_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
