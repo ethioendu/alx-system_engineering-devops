443bf9f0d8aeb34b7696d9a0afaf31014386bc45
root <ethioendu@git.com>
2024-03-25 13:18:48 -0700

    p

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install flask from pip3
 # Install a specific version of flsk(2.1.0)
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+	ensure   => '2.1.0',
+	provider => 'pip'
 }
