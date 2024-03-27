20cbe2f19b2747204aa8bfb4ecd6e597d22bcdc4
root <ethioendu@git.com>
2024-03-27 01:32:06 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,8 @@
 #with specific permissions, owner, group, and content
 file { '/tmp/school':
   ensure  => file,
-  mode  => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content  => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
