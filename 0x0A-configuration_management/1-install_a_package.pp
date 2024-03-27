58081139bd300567167bdd5042e50368ab8054f7
root <ethioendu@git.com>
2024-03-27 01:34:48 -0700

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-# Using Puppet, install flask from pip3
-# Install a specific version of flsk(2.1.0)
+# Puppet Manifest: Install Flask version 2.1.0 and Werkzeug version 2.1.1
+
 package { 'flask':
-	ensure  => '2.1.0',
-	provider => 'pip'
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
-
-# Using Puppet, create a manifest that kills a process named killmenow.
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-    provider => 'shell',
- 
+# kill the process: 'killmenow'
+exec {'kill_killmenow_process':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
