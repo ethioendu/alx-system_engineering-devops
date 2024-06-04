47eeebd92726151eaa047a379b36458efd41a9b1
ethioendu <ethioendu21@yahoo.com>
2024-06-04 09:39:39 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
+"""a module with one method
+"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
-    else:
-        return(0)
+    """ gets number of subscribers"""
+    subscribers_source = requests.get(
+        f'https://www.reddit.com/r/{subreddit}/about.json')
+    if subscribers_source.status_code >= 300:
+        return 0
+    getall = subscribers_source.json()
+    return getall.get("data").get("subscribers")
