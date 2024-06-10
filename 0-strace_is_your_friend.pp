2f94b9e59ad09f07325dee44729078d8e28e9164
ethioendu <ethioendu21@yahoo.com>
2024-06-10 06:44:46 -0700

    so

--- 0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# why Apache is returning a 500 error and fix it and then automate it using Puppet (
-
-exec { 'fix typo':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
-}
